0b0de1c4ea6ec43eb306c9595aa18dccf7d7df3b
john06patrick20 <johnpatrickcamposano@gmail.com>
2026-02-17 11:43:57 +0800

    Fix SSL database configuration for Render

--- athletitrack/settings.py
+++ athletitrack/settings.py
@@ -97,7 +97,10 @@ WSGI_APPLICATION = 'athletitrack.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 
